e9b9cc5232b3d9d2509bb6f4998ee5de849b9d0d
LeVietDuc2000 <levietduc2000cb@gmail.com>
2022-05-20 11:22:57 +0700

    Sửa và thêm header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -7,18 +7,40 @@ import {
   faCircleXmark,
   faSpinner,
   faMagnifyingGlass,
+  faEllipsisVertical,
+  faKeyboard,
+  faCircleQuestion,
+  faLanguage,
 } from "@fortawesome/free-solid-svg-icons";
 
+import Button from "~/components/Button";
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
 import { Wrapper as PopperWrapper } from "~/components/Popper";
 
 import { useEffect, useState } from "react";
 import AccountItem from "~/components/AccountItem";
+import Menu from "~/components/Popper/Menu";
 
 const cx = classNames.bind(styles);
 function Header() {
   const [searchResult, setSearchResult] = useState([]);
+  const MENU_ITEMS = [
+    {
+      icon: <FontAwesomeIcon icon={faLanguage} />,
+      title: "English",
+    },
+    {
+      icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+      title: "Feedback and help",
+      to: "/feedback",
+    },
+    {
+      icon: <FontAwesomeIcon icon={faKeyboard} />,
+      title: "Keyboard shortcuts",
+    },
+  ];
+
   useEffect(() => {
     const setTimeoutSearch = setTimeout(() => {
       setSearchResult([]);
@@ -62,7 +84,16 @@ function Header() {
             </button>
           </div>
         </Tippy>
-        <div className={cx("actions")}></div>
+        <div className={cx("actions")}>
+          <Button text>Upload</Button>
+          <Button primary>Log in</Button>
+          <Menu items={MENU_ITEMS}>
+            <button className={cx("more-btn")}>
+              <FontAwesomeIcon icon={faEllipsisVertical} />
+            </button>
+          </Menu>
+          ;
+        </div>
       </div>
     </header>
   );
